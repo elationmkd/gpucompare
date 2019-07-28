142624b49d5dbce792f049592e309d1129a3fc01
Mihajlo <mihajlobt@gmail.com>
2019-07-28 22:00:17 +0200

    fixed comparison, clear dom elements properly

--- javas.js
+++ javas.js
@@ -1,24 +1,137 @@
-var data =[];
-var button = document.getElementById("btn");
+let data = [];
+let firstGPUData = {};
+let secondGPUData = {};
 
-btn.addEventListener("click", fn1);
+const minCharacterLimit = 3;
 
-function fn1() {
-  var str = document.getElementById("text1").value;
-  getGpu(str);
-  console.log(str);
-}
+const firstInput = document.querySelector("#autocompleteInput1");
+const secondInput = document.querySelector("#autocompleteInput2");
 
-function getGpu(input) {
-  fetch("https://gist.githubusercontent.com/vjakovlev/c557267be3377473cbd2ebabdc1cb1ba/raw/7becef2faa7e1adc05bd6a130d8700e18b0c5480/gc.json")
-  .then(response => response.json())
-  .then(result => {
-    data.push(result)
-  })
-}
+const firstInputContainer = document.querySelector("#autocompleteContainer1");
+const secondInputContainer = document.querySelector("#autocompleteContainer2");
 
-for(let i=0; i <data.length;i++){
-  console.log(data[i])
-}
+const autocompleteResults = document.createElement("ul");
 
-console.log(data);
+
+const compareBtn = document.querySelector(".compare-btn");
+const clearBtn = document.querySelector(".clear-btn");
+
+const firstGPUStatsDOM = document.querySelector(".first-gpu");
+const secondGPUStatsDOM = document.querySelector(".second-gpu");
+
+const getData = () => {
+    fetch('https://gist.githubusercontent.com/vjakovlev/c557267be3377473cbd2ebabdc1cb1ba/raw/7becef2faa7e1adc05bd6a130d8700e18b0c5480/gc.json')
+        .then(response => {
+            response.json().then(result => {
+                data = result;
+                console.log(data)
+            });
+        })
+};
+
+const searchData = keyword => {
+    return data.filter(item => {
+        if (item.name.toLowerCase().includes(keyword.toLowerCase())) {
+            return item
+        }
+    })
+};
+
+const clearResults = () => {
+    if (autocompleteResults && autocompleteResults.children.length) {
+        autocompleteResults.innerHTML = "";
+        autocompleteResults.parentElement.removeChild(autocompleteResults)
+    }
+};
+
+const clearGPUStats = () => {
+    firstGPUStatsDOM.innerHTML = "";
+    secondGPUStatsDOM.innerHTML = "";
+};
+
+const appendResults = (results, inputName) => {
+
+    clearResults();
+
+    for (let i = 0; i < results.length; i++) {
+        const resultItem = results[i];
+        const resultDOMItem = document.createElement("li");
+        resultDOMItem.innerHTML = resultItem.name;
+        resultItem.className = `${resultItem.id} resultItem`;
+        resultDOMItem.addEventListener("click", ()=>{
+            clearResults(autocompleteResults);
+            if (inputName === 'firstInput') {
+                firstGPUData = resultItem;
+                firstInput.value = resultItem.name
+            } else {
+                secondGPUData = resultItem;
+                secondInput.value = resultItem.name;
+            }
+        });
+        autocompleteResults.appendChild(resultDOMItem)
+
+    }
+
+    if (inputName === 'firstInput') {
+        firstInputContainer.appendChild(autocompleteResults);
+    } else {
+        secondInputContainer.appendChild(autocompleteResults);
+    }
+};
+
+const onInputChange = event => {
+        const inputValue = event.target.value;
+
+        if (inputValue && inputValue.length >= minCharacterLimit) {
+            const searchResult = searchData(inputValue);
+            appendResults(searchResult, event.target.name);
+        } else {
+            clearResults(autocompleteResults)
+        }
+};
+
+const compareGPU = event => {
+    if (!Object.keys(firstGPUData).length || !Object.keys(secondGPUData).length) {
+        return false;
+    }
+    clearGPUStats();
+    renderComparison('firstGPUData');
+    renderComparison('secondGPUData');
+};
+
+const renderComparison = gpuDataContainerName => {
+    const results = [];
+    const gpuData = gpuDataContainerName === 'firstGPUData' ? firstGPUData : secondGPUData;
+    const gpuContainer = gpuDataContainerName === 'firstGPUData' ? firstGPUStatsDOM : secondGPUStatsDOM;
+    for (let key in gpuData) {
+        const row = document.createElement("div");
+        row.className = "comparison-row";
+
+        const dataKey = document.createElement("div");
+        dataKey.innerHTML = key;
+
+        const dataValue = document.createElement("div");
+        dataValue.innerHTML = gpuData[key];
+
+        row.appendChild(dataKey);
+        row.appendChild(dataValue);
+
+        results.push(row);
+        gpuContainer.appendChild(row);
+    }
+};
+
+const setEventListeners = element => {
+    element.addEventListener("input", onInputChange);
+    // element.addEventListener("blur", clearResults);
+    compareBtn.addEventListener("click", compareGPU);
+    clearBtn.addEventListener("click", ()=>{
+        clearGPUStats();
+    });
+};
+
+window.onload = () => {
+    setEventListeners(firstInput);
+    setEventListeners(secondInput);
+    getData();
+};
